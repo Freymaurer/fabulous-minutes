7b4353cae6d1b94aa361fb8657876b2f5d18bd30
Kevin F <Freymaurer@gmx.de>
2022-04-29 08:55:35 +0200

    add dynamic access logic

---
+++ src/fabulous-minutes.core/DynamicAccess.fs
@@ -0,0 +1,70 @@
+﻿namespace FabulousMinutes.Core
+
+open System
+open DynamicObj
+
+module DynamicAccess =
+
+    open System.Text.RegularExpressions
+
+    module Regex =
+        
+        /// 1. negative lookbehind: (?<!(/|\\)) -> No / or \ before {
+        /// 2. must start with: {
+        /// 3. capture named group 'value' : (?<value>.+?(?!(/|\\))); careful \<value> does not show as comment, better look at code.
+        /// 4. group contains any number of wildcard characters except { AND }, minimum 1 but as few as possible: [^\{}]+?
+        /// 5. negative lookahead: (?!(/|\\)) -> No / or \ before }
+        /// 6. must end with: }
+        [<Literal>]
+        let internal Pattern = @"(?<!(/|\\)){(?<value>[^\{}]+?(?!(/|\\)))}"
+
+
+        let getDynamicAccessStrings (input: string) = [| for i in Regex.Matches(input, Pattern) -> i |] 
+
+    let dynamicAccess (accessStr:string) (dynObject:DynamicObj) =
+        let toDynArr = accessStr.Split([|"."|], StringSplitOptions.RemoveEmptyEntries)
+        let rec access (ind:int) (dynArr:string []) result =
+            if ind >= dynArr.Length then
+                result
+            elif ind <> 0 && result = None then
+                None
+            else
+                let parentObj = if ind = 0 then dynObject else box result.Value :?> DynamicObj
+                let next = parentObj.TryGetValue(dynArr.[ind])
+                access (ind+1) dynArr next
+        access 0 toDynArr None
+
+    let readDynObjInFormatString(dynObj:DynamicObj,formatString:string) =
+        /// Need replacerList to store arbitrary guids and actual dynamic access values. 
+        /// The Guids are used as temporary replacements to remove escaped curly braces, without accidentally touching any inserted dynamic values.
+        let mutable replacerList: (string*string) list = []
+        let evaluator = 
+            MatchEvaluator (fun m -> 
+                let dynAccessResult = dynamicAccess m.Groups.["value"].Value (dynObj) 
+                let dynAccessResultString =
+                    if dynAccessResult.IsSome then 
+                        dynAccessResult.Value.ToString()
+                    else
+                        "None"
+                let newGuid = System.Guid.NewGuid().ToString()
+                // save both guid and actual value in replacerList. 
+                replacerList <- (newGuid,dynAccessResultString)::replacerList
+                // return guid to replace dynamic access string
+                newGuid
+            )
+        let removeEscapedCurlyBraces(str:string) = 
+            Regex.Replace(str, @"(\\{|/{)", @"{")
+            |> fun x -> Regex.Replace(x, @"(\\}|/})", @"}")
+        let replaceTempGuids(str:string) = 
+            let mutable res = str
+            replacerList |> List.iter (fun (guid,value) -> 
+                res <- Regex.Replace(res, guid, value)
+            )
+            res
+        // replace dyn access string with random guids, stored with actual values in replacerList
+        Regex.Replace(formatString, Regex.Pattern, evaluator)
+        // Update escaped curly braces to normal curly braces
+        |> removeEscapedCurlyBraces
+        // replace guids with actual dynamic access values
+        |> replaceTempGuids
+
