8fc916b4b79889c6197a7071348ef832e5864eb1
Kevin F <Freymaurer@gmx.de>
2022-05-05 15:21:07 +0200

    Add seqhead logic to dynamic access :tada:

--- src/fabulous-minutes.core/DynamicAccess.fs
+++ src/fabulous-minutes.core/DynamicAccess.fs
@@ -1,4 +1,4 @@
-﻿namespace FabulousMinutes.Core
+﻿namespace FabulousMinutes
 
 open System
 open DynamicObj
@@ -18,9 +18,10 @@ module DynamicAccess =
         [<Literal>]
         let internal Pattern = @"(?<!(/|\\)){(?<value>[^\{}]+?(?!(/|\\)))}"
 
-
         let getDynamicAccessStrings (input: string) = [| for i in Regex.Matches(input, Pattern) -> i |] 
 
+    /// Use "." syntax to access nested objects. Use "^?" to access sequence header.
+    /// **Example**: "Request.Body^?"
     let dynamicAccess (accessStr:string) (dynObject:DynamicObj) =
         let toDynArr = accessStr.Split([|"."|], StringSplitOptions.RemoveEmptyEntries)
         let rec access (ind:int) (dynArr:string []) result =
@@ -29,21 +30,35 @@ module DynamicAccess =
             elif ind <> 0 && result = None then
                 None
             else
+                // when starting the start object is the parent
                 let parentObj = if ind = 0 then dynObject else box result.Value :?> DynamicObj
-                let next = parentObj.TryGetValue(dynArr.[ind])
+                // next will be the result if it is the last step of the dynArr. 
+                // If it is not, it MUST be a dynamic object (Thats why parentObj will be casted to DynamicObj in the next rec iteration).
+                // If the current dynArr-step ends with "^?" the user assumes it is a sequence at this step and wants access to the seq head. In this case we cast to seq<'a>.
+                let currentDynArr, isSeqHead = 
+                    let cda0 = dynArr.[ind]
+                    let isSeqHead = cda0.EndsWith "^?" 
+                    let cda = if isSeqHead then cda0.[..cda0.Length-3] else cda0
+                    cda, isSeqHead
+                let next = 
+                    let next0 = parentObj.TryGetValue(currentDynArr)
+                    next0 |> Option.map (fun (n: obj) -> if isSeqHead then n :?> seq<obj> |> Seq.head else n) 
                 access (ind+1) dynArr next
         access 0 toDynArr None
 
     let readDynObjInFormatString(dynObj:DynamicObj,formatString:string) =
-        /// Need replacerList to store arbitrary guids and actual dynamic access values. 
+        /// Need replacerList to store artificial guids and actual dynamic access values. 
         /// The Guids are used as temporary replacements to remove escaped curly braces, without accidentally touching any inserted dynamic values.
         let mutable replacerList: (string*string) list = []
         let evaluator = 
             MatchEvaluator (fun m -> 
                 let dynAccessResult = dynamicAccess m.Groups.["value"].Value (dynObj) 
                 let dynAccessResultString =
-                    if dynAccessResult.IsSome then 
-                        dynAccessResult.Value.ToString()
+                    if dynAccessResult.IsSome then
+                        match dynAccessResult.Value with
+                        | :? DynamicObj as d -> DynamicObj.toJson d
+                        | :? seq<obj> as dseq -> dseq |> Seq.map (fun x -> x :?> DynamicObj |> DynamicObj.toJson) |> String.concat "; " |> sprintf "[%s]"
+                        | _ -> dynAccessResult.Value.ToString()
                     else
                         "None"
                 let newGuid = System.Guid.NewGuid().ToString()
